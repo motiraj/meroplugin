6c8924a98bc965581cd54efedda15cdf2d9c1e1b
motiraj <gautam.motiraj@gmail.com>
2024-06-20 16:50:37 +0545

    Version 1.0.0 Second Draft

--- database.php
+++ database.php
@@ -8,12 +8,13 @@ $results = $wpdb->get_results("SELECT * FROM wp_comments where comment_type='fee
 // Check if there are any results
 if ($results) {
 	echo '<table>';
-	echo '<tr><th>Name</th><th>Email</th><th>Feedback</th></tr>';
+	echo '<tr><th>Comment ID </th><th>Name</th><th>Email</th><th>Feedback</th></tr>';
 	foreach ($results as $row) {
 		echo '<tr>';
+		echo '<td>' . esc_html($row->comment_ID) . '</td>';
 		echo '<td>' . esc_html($row->comment_author) . '</td>';
 		echo '<td>' . esc_html($row->comment_author_email) . '</td>';
-		echo '<td>' . esc_html($row->comment_content) . '</td>';
+		echo '<td><input type="text" value="' . esc_html($row->comment_content) . '"</td>';
 		echo '</tr>';
 	}
 	echo '</table>';
